18e165a6e9ea02bdf8bca5e5daff49eb9c07480b
Theodore Keloglou <theodorekeloglou@gmail.com>
2016-02-05 01:15:37 +0200

    start server on sample test

--- test/index.js
+++ test/index.js
@@ -6,6 +6,12 @@ var expect = chai.expect;
 var req = require('request');
 
 describe('Frontpage', function() {
+  before(function (done) {
+    var app = require('../back/app');
+    app.listen(3000);
+    done();
+  });
+
   it('should get a 200 on the home page', function(done) {
     req.get('http://localhost:3000/', function(err, res) {
       expect(res.statusCode).to.equal(200);
