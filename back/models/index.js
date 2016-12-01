7ec7d3b5521badaf04f6dc152da04cbbe804d5e4
Theodore Keloglou <theodorekeloglou@gmail.com>
2016-12-01 17:40:55 +0200

    switch db connect to url

--- back/models/index.js
+++ back/models/index.js
@@ -8,9 +8,9 @@ const config = require('config');
 const Sequelize = require('sequelize');
 
 const configPostgres = config.get('postgres');
-const sequelize = new Sequelize(configPostgres.database, configPostgres.uername, configPostgres.password, {
-  host: configPostgres.host,
-  dialect: 'postgres',
+
+const sequelize = new Sequelize(config.postgres.url, {
+    logging: false,
 });
 
 const user = sequelize.import(path.join(__dirname, 'user.model.js'));
