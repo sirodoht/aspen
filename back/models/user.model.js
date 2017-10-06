02fd7f08aa1ee4cfdf33e66613da31c16b53b3eb
Theodore Keloglou <theodorekeloglou@gmail.com>
2017-10-07 02:37:41 +0300

    update user model to sequelize v4

--- back/models/user.model.js
+++ back/models/user.model.js
@@ -3,8 +3,8 @@
  */
 
 const Promise = require('bluebird');
-const Sequelize = require('sequelize');
 const bcrypt = require('bcrypt');
+
 
 Promise.promisifyAll(require('bcrypt'));
 
@@ -21,46 +21,25 @@ module.exports = function (sequelize, DataTypes) {
         isEmail: true,
       },
     },
-
-    lastLogin: Sequelize.DATE,
-    lastIp: {
-      type: Sequelize.STRING,
-      validate: {
-        isIP: true,
-      },
-    },
-
-    website: {
-      type: DataTypes.STRING,
-      validate: {
-        isUrl: true,
-      },
-    },
-    location: DataTypes.STRING,
   };
 
-  const options = {};
-  options.classMethods = {
-    associate: function (models) {
-      User.hasMany(models.Course);
-    }
-  };
-  options.instanceMethods = {
-    validPassword: function (pwd) {
-      return bcrypt.compareAsync(pwd, this.password).then(function (isMatch) {
+
+  const User = sequelize.define('User', attributes);
+
+  User.prototype.validPassword = function (pwd) {
+    return bcrypt.compareAsync(pwd, this.password)
+      .then(function (isMatch) {
         return isMatch;
       });
-    },
   };
-
-  const User = sequelize.define('User', attributes, options);
 
   const hashPasswordHook = function (user) {
     return bcrypt.genSaltAsync(SALT_WORK_FACTOR)
       .then(function (salt) {
-        return bcrypt.hashAsync(user.password, salt).then(function (hash) {
-          user.password = hash;
-        });
+        return bcrypt.hashAsync(user.password, salt)
+          .then(function (hash) {
+            user.password = hash;
+          });
       })
       .catch(function (err) {
         console.log('Password hook error:', err);
